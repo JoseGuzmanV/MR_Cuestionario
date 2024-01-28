fd267cfabe5f2dbedd31d838ec75720b091d5e48
CODER-JGuzman <150303516+JoseGuzmanV@users.noreply.github.com>
2024-01-28 14:20:53 -0500

    Add files via upload

---
+++ 06_UC_G5_survey_queries.sql
@@ -0,0 +1,22 @@
+Tiempo de encuesta, ENERO 2023 - ENERO 2024:
+
+1.	¿Cuales son las líneas de ventas con mayor satisfacción (prom >=4) de usuarios en el último trimestre?
+
+2.	¿Cuales son las zonas de ventas con mayor satisfacción (prom >=4) de usuarios en el último trimestre?
+
+3.	¿Cuales son las líneas de ventas con mayor insatisfacción (prom <2) de usuarios en este último mes?
+
+4.	Porcentaje de clientes varones menores de 20 años con mayor satisfacción (prom >= 4) en la línea MODA en noviembre 2023?
+
+5.	En el último mes-calendario ¿cuál fue la línea de ventas con clientes mujeres menores de 30 años mas satisfechas (prom >=4)? 
+
+6.	En el último mes-calendario ¿Cuál es el promedio del nivel de satisfechos de la calidad del producto de la línea BLANCA?
+
+7.	En el último trimestre ¿Cuál es el porcentaje de clientes que nos recomiendan (prom >=4)? 
+
+8.	En el último semestre ¿Cuál es el el nivel de insatisfechos (prom <=2) de clientes mujeres en la linea BELLEZA?
+
+9.	¿Cuál es el porcentaje de usuarios insatisfechos (prom <=2) de cada zona?
+
+10.	¿Cuál es el promedio del nivel de satisfacción el servicio de personas mayores de 65 años en la línea BLANCA durante el último mes?
+
